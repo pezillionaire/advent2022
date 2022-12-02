0daeca3d41117953b06e9ba8b3b30830767ff8f5
pezillionaire <pezpengelly@gmail.com>
2022-12-02 09:04:28 -0800

    2022-12-01

---
+++ 2022-12-01/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const elfList = fs.readFileSync('input.txt').toString().split("\n\n");
+const topElves = [];
+const elfSums = []
+for (i in elfList) {
+  const elf = (elfList[i].split("\n"));
+  for (i in elf) { elf[i] = parseInt(elf[i]); }
+  const sum = elf.reduce((partialSum,a) => partialSum + a,0);
+  if (!Number.isNaN(sum)) { elfSums.push(sum); }
+}
+[...Array(3)].forEach((x,i) => {
+  let top = elfSums.indexOf(Math.max(...elfSums));
+  console.log(i + 1,top);
+  topElves.push(elfSums[top])
+  elfSums.splice(top,1)
+});
+console.log('top 3',topElves);
+console.log('sum',topElves.reduce((partialSum,a) => partialSum + a,0));
