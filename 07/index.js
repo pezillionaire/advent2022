c2b9ac7d0f62fb00ab9ae2b6541a894b2f18173f
pezillionaire <pezpengelly@gmail.com>
2022-12-07 23:46:24 -0800

    Day 7 - sigh
    demo data validates but result from input is wrong.

--- 07/index.js
+++ 07/index.js
@@ -1,33 +1,45 @@
-
 const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().split(/\r?\n/);
-let nest = [];
-let structure = { '/': {} };
-let cd = '';
+const nest = [];
+const dirStructure = { '/': {} };
+const dirSize = {};
+let curDir = '';
 
-const setNestedKey = (obj,path,value) => {
+const setNestedValue = (obj,path,value) => {
   if (path.length === 1) {
     obj[path] = value
     return
   }
-  return setNestedKey(obj[path[0]],path.slice(1),value)
+  return setNestedValue(obj[path[0]],path.slice(1),value)
 }
 
-input.forEach((line,i) => {
+input.forEach((line) => {
   const cmd = line.split(/\s/);
-  if (cmd[0] == '$') {
+  if (cmd[0] == '$') { // check if command
     if (cmd[1] == 'cd') {
-      if (cmd[2] == '..') {
+      if (cmd[2] == '..') { // remove from nesting
         nest.pop();
-      } else { cd = cmd[2]; }
-    } else if (cmd[1] == 'ls') {
-      nest.push(cd);
+      } else { curDir = cmd[2]; } // set current directory
+    } else if (cmd[1] == 'ls') { // set current directory to nesting
+      nest.push(curDir);
     }
-  } else if (parseInt(cmd[0])) {
-    setNestedKey(structure,[...nest,cmd[1]],cmd[0])
-  } else if (cmd[0] == 'dir') {
-    setNestedKey(structure,[...nest,cmd[1]],{})
+  } else if (parseInt(cmd[0])) { // set files w/ size
+    setNestedValue(dirStructure,[...nest,cmd[1]],cmd[0]);
+    for (i of nest) {
+      if (dirSize[i]) {
+        dirSize[i] += parseInt(cmd[0]);
+      } else { dirSize[i] = parseInt(cmd[0]); }
+    };
+  } else if (cmd[0] == 'dir') { // set dirs
+    setNestedValue(dirStructure,[...nest,cmd[1]],{});
   }
 });
 
-console.log(structure);
+let result = 0
+for (const [key,value] of Object.entries(dirSize)) {
+  if (value <= 100000) {
+    result += value;
+  }
+}
+console.log(dirSize);
+console.log('result',result);
